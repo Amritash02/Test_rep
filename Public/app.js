53dbcd572cc4deae4c7c2f1d8172fe988a890295
amritash02 <amrita.sharma@okstate.edu>
2022-10-25 08:41:00 -0500

    Update app.js

--- Public/app.js
+++ Public/app.js
@@ -2,28 +2,11 @@
     var myConnector = tableau.makeConnector();
 
     myConnector.getSchema = function (schemaCallback) {
-        tableau.log("Hello WDC!");
-    };
-  
 
-    myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-            var feat = resp.features,
-                tableData = [];
-    
-            // Iterate over the JSON object
-            for (var i = 0, len = feat.length; i < len; i++) {
-                tableData.push({
-                    "id": feat[i].id,
-                    "mag": feat[i].properties.mag,
-                    "title": feat[i].properties.title,
-                    "location": feat[i].geometry
-                });
-            }
-    
-            table.appendRows(tableData);
-            doneCallback();
-        });
+    };
+
+    myConnector.getData = function (table, doneCallback) {
+
     };
 
     tableau.registerConnector(myConnector);
