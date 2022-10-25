46dc101254840553b00f847fe92e145e1f66fe6c
amritash02 <amrita.sharma@okstate.edu>
2022-10-25 08:43:20 -0500

    Update app.js

--- Public/app.js
+++ Public/app.js
@@ -10,10 +10,10 @@
     };
 
     tableau.registerConnector(myConnector);
-    $(document).ready(function () {
-        $("#submitButton").click(function () {
-            tableau.connectionName = "USGS Earthquake Feed";
-            tableau.submit();
-        });
-    });
 })();
+$(document).ready(function () {
+    $("#submitButton").click(function () {
+        tableau.connectionName = "USGS Earthquake Feed";
+        tableau.submit();
+    });
+});
