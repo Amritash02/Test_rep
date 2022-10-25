b0fe822ee4a5a2388c5e4b2315007b10f78dc00a
amritash02 <amrita.sharma@okstate.edu>
2022-10-25 10:00:54 -0500

    added

--- Public/app.js
+++ Public/app.js
@@ -1,58 +1,81 @@
+console.log("This is working!");
+
 (function () {
-    var myConnector = tableau.makeConnector();
+  var myConnector = tableau.makeConnector();
 
-    myConnector.getSchema = function (schemaCallback) {
-        var cols = [{
-            id: "id",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "mag",
-            alias: "magnitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "title",
-            alias: "title",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "location",
-            dataType: tableau.dataTypeEnum.geometry
-        }];
-    
-        var tableSchema = {
-            id: "earthquakeFeed",
-            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
-            columns: cols
-        };
-    
-        schemaCallback([tableSchema]);
+  myConnector.getSchema = function (schemaCallback) {
+    const covidCols = [
+      {
+        id: "Date_of_report",
+        dataType: tableau.dataTypeEnum.date,
+      },
+      {
+        id: "Municipality_code",
+        dataType: tableau.dataTypeEnum.string,
+      },
+      {
+        id: "Municipality_name",
+        dataType: tableau.dataTypeEnum.string,
+      },
+      {
+        id: "Province",
+        dataType: tableau.dataTypeEnum.string,
+      },
+      {
+        id: "Total_reported",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "Hospital_admission",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "Deceased",
+        dataType: tableau.dataTypeEnum.int,
+      },
+    ];
+
+    let covidTableSchema = {
+      id: "RIVM",
+      alias: "Dutch Corona Cases since start",
+      columns: covidCols,
     };
 
-    myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-            var feat = resp.features,
-                tableData = [];
-    
-            // Iterate over the JSON object
-            for (var i = 0, len = feat.length; i < len; i++) {
-                tableData.push({
-                    "id": feat[i].id,
-                    "mag": feat[i].properties.mag,
-                    "title": feat[i].properties.title,
-                    "location": feat[i].geometry
-                });
-            }
-    
-            table.appendRows(tableData);
-            doneCallback();
-        });
-    };
+    schemaCallback([covidTableSchema]);
+  };
 
-    tableau.registerConnector(myConnector);
+  myConnector.getData = function (table, doneCallback) {
+    let tableData = [];
+    var i = 0;
 
-    $(document).ready(function () {
-        $("#submitButton").click(function () {
-            tableau.connectionName = "USGS Earthquake Feed";
-            tableau.submit();
-        });
-    });
+    $.getJSON(
+      "https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.json",
+      function (resp) {
+        // Iterate over the JSON object
+        for (i = 0, len = resp.length; i < len; i++) {
+          tableData.push({
+            Date_of_report: resp[i].Date_of_report,
+            Municipality_code: resp[i].Municipality_code,
+            Municipality_name: resp[i].Municipality_name,
+            Province: resp[i].Province,
+            Total_reported: resp[i].Total_reported,
+            Hospital_admission: resp[i].Hospital_admission,
+            Deceased: resp[i].Deceased,
+          });
+        }
+        table.appendRows(tableData);
+        doneCallback();
+      }
+    );
+  };
+
+  tableau.registerConnector(myConnector);
+  
 })();
+
+document.querySelector("#getData").addEventListener("click", getData);
+
+function getData() {
+  tableau.connectionName = "Dutch Corona Numbers";
+  tableau.submit();
+}
