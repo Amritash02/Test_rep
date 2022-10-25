4e4155a0a04f679237d92fa3754f9b4d823507bd
amritash02 <amrita.sharma@okstate.edu>
2022-10-24 23:49:34 -0500

    Update app.js

--- Public/app.js
+++ Public/app.js
@@ -2,30 +2,9 @@
     var myConnector = tableau.makeConnector();
 
     myConnector.getSchema = function (schemaCallback) {
-        var cols = [{
-            id: "id",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "mag",
-            alias: "magnitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "title",
-            alias: "title",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "location",
-            dataType: tableau.dataTypeEnum.geometry
-        }];
-    
-        var tableSchema = {
-            id: "earthquakeFeed",
-            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
-            columns: cols
-        };
-    
-        schemaCallback([tableSchema]);
+        tableau.log("Hello WDC!");
     };
+  
 
     myConnector.getData = function(table, doneCallback) {
         $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
